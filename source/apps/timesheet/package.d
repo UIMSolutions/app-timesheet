410c855c25fb845b49311a6be782b015aec79e26
UIMSolutions <ui.manufaktur@gmail.com>
2023-05-17 11:32:44 +0200

    Update

--- source/apps/timesheet/package.d
+++ source/apps/timesheet/package.d
@@ -1,5 +1,34 @@
 module apps.timesheets;
 
-public import apps.timesheets.controllers;
-public import apps.timesheets.routes;
-public import apps.timesheets.views;
+mixin(ImportPhobos!());
+
+// Dub
+public import vibe.d;
+
+// UIM
+public import uim.core;
+public import uim.bootstrap;
+public import uim.html;
+public import uim.oop;
+public import uim.models;
+public import uim.apps;
+public import uim.controls;
+public import uim.servers;
+
+public import langs.javascript;
+
+public {
+  import apps.timesheets.controllers;
+  import apps.timesheets.helpers;
+  import apps.timesheets.tests;
+  import apps.timesheets.views;
+}
+
+DApp timesheetsApp;
+static this() {
+  timesheetsApp = App
+    .name("timesheetsApp")
+    .rootPath("/apps/timesheets")
+    .addRoute(Route("", HTTPMethod.GET, TSHIndexPageController))
+    .addRoute(Route("/", HTTPMethod.GET, TSHIndexPageController));
+}
