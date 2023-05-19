4129ac7ee7a198595f2845a7d459a12ff2e9c081
UIMSolutions <ui.manufaktur@gmail.com>
2023-05-19 11:49:51 +0200

    Update

--- source/apps/timesheet/views/package.d
+++ source/apps/timesheet/views/package.d
@@ -1,4 +1,17 @@
-module apps.timesheets.views;
+/*********************************************************************************************************
+  Copyright: © 2015-2023 Ozan Nurettin Süel (Sicherheitsschmiede)                                        
+  License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
+  Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
+**********************************************************************************************************/
+module apps.myname.views;
 
-public import apps.timesheets.views.error;
-public import apps.timesheets.views.index;
+public { // Packages
+  import apps.myname.views.components;
+  import apps.myname.views.forms;
+  import apps.myname.views.pages;
+}
+
+public { // Views
+  import apps.myname.views.error;
+  import apps.myname.views.index;
+}
